ab4bde3321cd8711dc9ef58ad9c25053d4cd9405
trinity <ryan.cu1200@gmail.com>
2024-08-23 22:08:59 +0530

    added socket io

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("welcome to chatterBox")
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handle_message(data):
+    print('Received message: ' + data)
+    emit('response', 'Server received your message: ' + data)
+if __name__ == '__main__':
+    socketio.run(app)
