f8cd5948c3147f0b0b39e20a7f2d4be5aeea86b4
trinity <ryan.cu1200@gmail.com>
2024-08-24 23:16:47 +0530

    added home route for testing

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @socketio.on('message')
 def handle_message(data):
     print('Received message: ' + data)
     emit('response', 'Server received your message: ' + data)
+
+@socketio.on('json')
+def handle_json(json):
+    print('Received JSON: ' + str(json))
+    emit('response', 'Server received your JSON: ' + str(json))
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
