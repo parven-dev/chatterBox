795e0c70abf9951270329730fb1470d0a14c5f18
trinity <ryan.cu1200@gmail.com>
2024-09-01 22:46:30 +0530

    connected login system to db

--- routes/login.py
+++ routes/login.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from db.database import User
 
 login = Blueprint("login", __name__, url_prefix="/login")
+
 
 @login.route("/", methods=["GET", "POST"])
 def login_view():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["pass"]
+        user = User.query.filter_by(email=email).first()
+        if user and password == user.password:
+            return "you login successfully"
         # Perform login logic here
-        return redirect(url_for("login.login_view"))
+        else:
+            return redirect(url_for("login.login_view"))
     return render_template("login.html")
