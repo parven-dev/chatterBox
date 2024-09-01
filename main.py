c4cb071c0be034f9e4de0b23081e0390cf1fe4f3
trinity <ryan.cu1200@gmail.com>
2024-09-01 23:02:07 +0530

    added flask_login system

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.signup import signup
 from routes.index import index
 from socketio_seteup.socketio_setup import socketio
 from db.database import db
+from routes.login import login_manager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
@@ -12,13 +13,13 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 # configure the SQLite db, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
+login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
 
 
 socketio.init_app(app)
-
 
 app.register_blueprint(login)
 app.register_blueprint(signup)
--- routes/login.py
+++ routes/login.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from db.database import User
 
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 login = Blueprint("login", __name__, url_prefix="/login")
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 
 @login.route("/", methods=["GET", "POST"])
@@ -11,6 +20,7 @@ def login_view():
         password = request.form["pass"]
         user = User.query.filter_by(email=email).first()
         if user and password == user.password:
+            login_user(user)
             return "you login successfully"
         # Perform login logic here
         else:
