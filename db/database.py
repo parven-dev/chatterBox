40e111e772da2f370f6cd8e926477560b8642c3a
trinity <ryan.cu1200@gmail.com>
2024-09-08 22:49:02 +0530

    changed in database

--- db/database.py
+++ db/database.py
@@ -1,18 +1,18 @@
-from main import app
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(200))
     password = db.Column(db.String(200))
-
+    #
     # def __init__(self, email, password):
     #     self.email = email
     #     self.password = password
 
 
-with app.app_context():
-    db.create_all()
+
+
